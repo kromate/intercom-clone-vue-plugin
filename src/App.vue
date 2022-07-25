13a3152c3158208c31c9e159e314e63880c56b68
kromate <akpananthony33@gmail.com>
2022-07-25 22:57:10 +0100

    -

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <CareButton title="This is the Default" />
+</template>
+
+<script setup lang="ts">
+import CareButton from "./components/CareButton.vue";
+</script>
+
+<style scoped></style>
