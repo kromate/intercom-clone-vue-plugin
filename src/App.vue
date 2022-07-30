c14caae6d37efd83d8beb89438a8f67a391a0667
kromate <akpananthony33@gmail.com>
2022-07-30 18:07:12 +0100

    -

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import CareButton from "./components/CareButton.vue";
+import CareButton from "./components/CareButton/index.vue";
 </script>
 
 <style scoped></style>
